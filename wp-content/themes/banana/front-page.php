59f51c471b2e9b3152f726d342fc1ab5be0f2562
aincatoni <ain.catoni@gmail.com>
2019-03-30 23:15:10 -0300

    Decimo commit: pasando la estructura de index.php a front-page.php

---
+++ wp-content/themes/banana/front-page.php
@@ -0,0 +1,28 @@
+  <!-- Header -->
+  <?php get_header();?>
+
+  <!-- Main Content -->
+  <main>
+    
+    <!-- Banana Sesiones -->
+    <section></section>
+    
+    <!-- Eventos Banana -->
+    <section></section>
+    
+    <!-- Últimas noticias  -->
+    <section></section>
+    
+    <!-- Bandas Banana -->
+    <section></section>
+
+    <!-- Nosotros -->
+    <section></section>
+
+    <!-- RRSS -->
+    <section></section>
+  
+  </main> 
+    
+  <!-- Footer -->
+  <?php get_footer();?>
