ef3adeb1e09f0b94036955420e7380e13ddfa7a5
aincatoni <ain.catoni@gmail.com>
2019-03-31 18:41:28 -0300

    Commit 18: armando la estructura de los custom post de banana sesiones en el front-page.php

--- wp-content/themes/banana/front-page.php
+++ wp-content/themes/banana/front-page.php
@@ -5,22 +5,99 @@
   <main>
     
     <!-- Banana Sesiones -->
-    <section id="bananaSesiones"></section>
+    <section id="bananaSesiones">
+      <div class="container-fluid mt-3"> 
+          <div class="row">
+            <div class="col-12 text-center">
+              <a href="">
+                <img src="https://via.placeholder.com/2000x200.png?text=Banana+Sesiones" class="img-fluid" alt="">
+              </a>
+            </div>
+          </div> 
+      </div>
+      <!-- LOOP CPT SESIONES -->
+      <div class="container-fluid my-5">
+        <div class="row">
+          <div class="col-4">
+          <img class="img-fluid" src="https://via.placeholder.com/400x400.png" alt="">
+          <h3>POST 1</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam et sequi vel.</p>
+          <span><a href="#">Revisa la sesión aquí</a></span>
+          </div>
+          <div class="col-4">
+          <img class="img-fluid" src="https://via.placeholder.com/400x400.png" alt="">
+          </div>
+          <div class="col-4">
+          <img class="img-fluid" src="https://via.placeholder.com/400x400.png" alt="">
+          </div>  
+        </div>
+      </div>
+    </section>
     
     <!-- Eventos Banana -->
-    <section id="bananaEventos"></section>
+    <section id="bananaEventos">
+      <div class="container-fluid"> 
+            <div class="row">
+              <div class="col-12 text-center">
+                <a href="">
+                  <img src="https://via.placeholder.com/2000x200.png?text=Banana+Eventos" class="img-fluid" alt="">
+                </a>
+              </div>
+            </div> 
+      </div>
+    </section>
     
     <!-- Últimas noticias  -->
-    <section id="bananaNoticias"></section>
+    <section id="bananaNoticias">
+      <div class="container-fluid"> 
+          <div class="row">
+            <div class="col-12 text-center">
+              <a href="">
+                <img src="https://via.placeholder.com/2000x200.png?text=Banana+Noticias" class="img-fluid" alt="">
+              </a>
+            </div>
+          </div> 
+      </div>
+    </section>
     
     <!-- Bandas Banana -->
-    <section id="bananaBandas"></section>
+    <section id="bananaBandas">
+      <div class="container-fluid"> 
+          <div class="row">
+            <div class="col-12 text-center">
+              <a href="">
+                <img src="https://via.placeholder.com/2000x200.png?text=Banana+Bandas" class="img-fluid" alt="">
+              </a>
+            </div>
+          </div> 
+      </div>
+    </section>
 
     <!-- Nosotros -->
-    <section id="bananaNosotros"></section>
+    <section id="bananaNosotros">
+      <div class="container-fluid"> 
+          <div class="row">
+            <div class="col-12 text-center">
+              <a href="">
+                <img src="https://via.placeholder.com/2000x200.png?text=Banana+Nosotros" class="img-fluid" alt="">
+              </a>
+            </div>
+          </div> 
+      </div>
+    </section>
 
     <!-- RRSS -->
-    <section id="bananaSocial"></section>
+    <section id="bananaSocial">
+      <div class="container-fluid"> 
+          <div class="row">
+            <div class="col-12 text-center">
+              <a href="">
+                <img src="https://via.placeholder.com/2000x200.png?text=Banana+Social" class="img-fluid" alt="">
+              </a>
+            </div>
+          </div> 
+      </div>
+    </section>
   
   </main> 
     
