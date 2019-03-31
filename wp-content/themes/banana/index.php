67471c2486c882e4306c1f525030d8062cd9dee3
aincatoni <ain.catoni@gmail.com>
2019-03-30 22:22:57 -0300

    Septimo commit: creando la estructura básica de la página principal (home)

---
+++ wp-content/themes/banana/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Banana Colectivo</title>
+</head>
+<body>
+  
+  <!-- Header -->
+  <header>
+    <nav></nav>
+  </header>
+  
+  <!-- Main Content -->
+  <main>
+  
+    <!-- Banana Sesiones -->
+    <section></section>
+    
+    <!-- Eventos Banana -->
+    <section></section>
+    
+    <!-- Últimas noticias  -->
+    <section></section>
+    
+    <!-- Bandas Banana -->
+    <section></section>
+
+    <!-- Nosotros -->
+    <section></section>
+
+    <!-- RRSS -->
+    <section></section>
+    
+    <!-- Footer -->
+    <footer></footer>
+  
+  </main>
+
+</body>
+</html>
