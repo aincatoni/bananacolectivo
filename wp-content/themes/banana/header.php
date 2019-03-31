dc964a424430b764c39d76b7790549239a8135ea
aincatoni <ain.catoni@gmail.com>
2019-03-30 23:17:02 -0300

    commit 11: agregando estructura básica de navbar al archivo header.php

--- wp-content/themes/banana/header.php
+++ wp-content/themes/banana/header.php
@@ -10,5 +10,21 @@
   
   <!-- Header -->
   <header>
-    <nav></nav>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Banana Colectivo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Inicio</a>
+      <a class="nav-item nav-link" href="#">Banana Sesiones</a>
+      <a class="nav-item nav-link" href="#">Eventos</a>
+      <a class="nav-item nav-link" href="#">Noticias</a>
+      <a class="nav-item nav-link" href="#">Bandas</a>
+      <a class="nav-item nav-link" href="#">Nosotros</a>
+    </div>
+  </div>
+</nav>
   </header>
