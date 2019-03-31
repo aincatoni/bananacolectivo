c692664d44f0720dcce57e81d545ac3a575296b2
aincatoni <ain.catoni@gmail.com>
2019-03-31 17:10:40 -0300

    commit 14: agregando los metadatos con wp_head al archivo header.php, además se agregó la estructura básica del navbar/scrolly

--- wp-content/themes/banana/header.php
+++ wp-content/themes/banana/header.php
@@ -4,27 +4,51 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Banana Colectivo</title>
+  <title><?php bloginfo('name') ?></title>
+  <?php wp_head(); ?> 
 </head>
-<body>
+<body data-spy="scroll" data-target="#navbar-example2">
   
   <!-- Header -->
   <header>
+
+    <nav id="navbar-example2" class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#"><?php bloginfo('name') ?></a>
+    <ul class="nav nav-pills">
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#bananaSesiones">Banana Sesiones</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#bananaEventos">Banana Eventos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#bananaNoticias">Banana Noticias</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#bananaBandas">Banana Bandas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#bananaEventos">Banana Nosotros</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#bananaSocial">Banana Social</a>
+      </li>
+    </ul>
+  </nav>
+
     
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Banana Colectivo</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Inicio</a>
-      <a class="nav-item nav-link" href="#">Banana Sesiones</a>
-      <a class="nav-item nav-link" href="#">Eventos</a>
-      <a class="nav-item nav-link" href="#">Noticias</a>
-      <a class="nav-item nav-link" href="#">Bandas</a>
-      <a class="nav-item nav-link" href="#">Nosotros</a>
+    <div class="container-fluid">
+      
+        <div class="row">
+          <div class="col-12 text-center">
+            <a href="">
+              <img src="https://picsum.photos/2000/500" class="img-fluid" alt="">
+              
+            </a>
+          </div>
+        </div>
+      
     </div>
-  </div>
-</nav>
+  
   </header>
