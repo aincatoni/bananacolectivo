f9a28bf9232ede8122c4715b545dfa3e608c779a
aincatoni <ain.catoni@gmail.com>
2019-03-31 22:35:43 -0300

    Commit 19: agregando estilos y mejorando estructura para la sección de banana sesiones

--- wp-content/themes/banana/front-page.php
+++ wp-content/themes/banana/front-page.php
@@ -8,29 +8,82 @@
     <section id="bananaSesiones">
       <div class="container-fluid mt-3"> 
           <div class="row">
-            <div class="col-12 text-center">
+            <div class="col-12 text-center px-0">
               <a href="">
-                <img src="https://via.placeholder.com/2000x200.png?text=Banana+Sesiones" class="img-fluid" alt="">
+                <img src="https://via.placeholder.com/2000x400.png?text=Banana+Sesiones" class="img-fluid" alt="">
               </a>
             </div>
           </div> 
       </div>
       <!-- LOOP CPT SESIONES -->
-      <div class="container-fluid my-5">
+      <div class="container-fluid my-3 px-5">
         <div class="row">
-          <div class="col-4">
-          <img class="img-fluid" src="https://via.placeholder.com/400x400.png" alt="">
-          <h3>POST 1</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam et sequi vel.</p>
-          <span><a href="#">Revisa la sesión aquí</a></span>
+          <div class="col-12 col-sm-12 col-md-8 col-lg-8 extracto-sesion-grande">
+            <div class="extracto-sesion-grande-texto">  
+              <h3>POST 1</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam et sequi vel.</p>
+              <span><a href="#">Revisa la sesión aquí</a></span>
+            </div>
           </div>
-          <div class="col-4">
-          <img class="img-fluid" src="https://via.placeholder.com/400x400.png" alt="">
+        
+          <div class="col-12 col-sm-12 col-md-4 px-0">
+           <div class="row">
+            <div class="col-6 col-sm-6 col-md-12 extracto-sesion-medio">
+              <div class="extracto-sesion-medio-texto">
+                <h3>POST 2</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam et sequi vel.</p>
+                <span><a href="#">Revisa la sesión aquí</a></span>
+              </div>
+            </div>
+            
+            <div class="col-6 col-sm-6 col-md-12 extracto-sesion-medio">
+              <div class="extracto-sesion-medio-texto">
+                <h3>POST 3</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam et sequi vel.</p>
+                <span><a href="#">Revisa la sesión aquí</a></span>
+              </div>
+            </div>
+            </div>
           </div>
-          <div class="col-4">
-          <img class="img-fluid" src="https://via.placeholder.com/400x400.png" alt="">
-          </div>  
+        
         </div>
+
+        <div class="row">
+          
+          <div class="col-6 col-sm-3 col-md-3 col-lg-3 extracto-sesion-chico">
+            <div class="extracto-sesion-chico-texto">
+              <h3>POST 4</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam et sequi vel.</p>
+              <span><a href="#">Revisa la sesión aquí</a></span>
+            </div>
+          </div>
+
+          <div class="col-6 col-sm-3 col-md-3 col-lg-3 extracto-sesion-chico">
+            <div class="extracto-sesion-chico-texto">
+              <h3>POST 5</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam et sequi vel.</p>
+              <span><a href="#">Revisa la sesión aquí</a></span>
+            </div>
+          </div>
+
+          <div class="col-6 col-sm-3 col-md-3 col-lg-3 extracto-sesion-chico">
+            <div class="extracto-sesion-chico-texto">
+              <h3>POST 6</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam et sequi vel.</p>
+              <span><a href="#">Revisa la sesión aquí</a></span>
+            </div>
+          </div>
+          
+          <div class="col-6 col-sm-3 col-md-3 col-lg-3 extracto-sesion-chico">
+            <div class="extracto-sesion-chico-texto">
+              <h3>POST 7</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam et sequi vel.</p>
+              <span><a href="#">Revisa la sesión aquí</a></span>
+            </div>
+          </div>
+        
+        </div>
+        
       </div>
     </section>
     
@@ -44,6 +97,13 @@
                 </a>
               </div>
             </div> 
+      </div>
+      <div class="container-fluid">
+        <div class="container">
+          <div class="row">
+          .
+          </div>
+        </div>
       </div>
     </section>
     
